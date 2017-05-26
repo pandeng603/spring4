2d43fd1557270730598a7b3725cd8debb978080e
pand <675757144@qq.com>
2017-05-26 23:34:01 +0800

    update 2017-05-26 23:33

---
+++ src/main/java/com/pand/highlight_spring4/ch3/aware/AwareService.java
@@ -0,0 +1,35 @@
+package com.pand.highlight_spring4.ch3.aware;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AwareService implements BeanNameAware,ResourceLoaderAware{
+	
+	private String beanName;
+	private ResourceLoader loader;
+	
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.loader = resourceLoader;
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		this.beanName = name;
+	}
+
+	public void outputResult() {
+		System.out.println("Bean的名称为:" + beanName);
+		Resource resource = loader.getResource("classpath:com/pand/highlight_spring4/ch3/aware/test.txt");
+		try {
+			System.out.println("ResourceLoader加载的文件内容是："+IOUtils.toString(resource.getInputStream(),"utf-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
